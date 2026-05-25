d6da208da2b24044b4cfa848a68860258fd1c4f4
Divyam <rajdivyamcpr@gmail.com>
2026-05-26 02:51:04 +0530

    Fix: trust proxy in express and set proxy: true in passport Google strategy to generate https redirect URLs

--- config/passport.js
+++ config/passport.js
@@ -21,6 +21,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "/api/oauth/google/callback",
+      proxy: true,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
+app.set("trust proxy", true);
 const cors = require("cors");
 const path = require("path");
 const session = require("express-session");
